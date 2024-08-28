1150446e251ba3b5a1ef24d06e3a5c76a799cb45
PengXinyang <148336099@qq.com>
2024-08-28 09:22:48 +0800

    V8.28

---
+++ discover_service/src/main/java/com/buaa13/bilimili/DiscoverApplication.java
@@ -0,0 +1,13 @@
+package com.buaa13.bilimili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class DiscoverApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(DiscoverApplication.class, args);
+        System.out.println(1);
+    }
+}
